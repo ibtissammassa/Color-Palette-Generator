4e8d54f9b3975320f6345635bf20f2df8b043e55
Hassan Elqadi <hassanelqadi2@gmail.com>
2023-03-10 00:25:01 +0100

    mid

---
+++ script.js
@@ -0,0 +1,21 @@
+const palettes = document.querySelector(".palettes");
+const refreshBtn = document.querySelector(".generate");
+const maxPalettes = 5;
+const generatePalette = () => {
+    for (i = 0; i< maxPalettes ; i++) {
+
+        //generating a random Hex color code 
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6,"0")}`;
+
+        //creating new li element and insering it to the palettes part
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `<div class="color-box" style="background: ${randomHex}"></div>
+        <span class="hex-value">${randomHex}</span>`
+        palettes.appendChild(color);
+    }
+
+}
+
+refreshBtn.addEventListener("click", generatePalette);
