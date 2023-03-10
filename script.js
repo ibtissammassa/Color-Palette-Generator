6130fc6075c063c9eda00de38ed01c7de7435204
Hassan Elqadi <hassanelqadi2@gmail.com>
2023-03-10 02:10:27 +0100

    ..

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const maxPalettes = 5;
 const generatePalette = () => {
     palettes.innerHTML = ""; //clearing the container
     for (i = 0; i< maxPalettes ; i++) {
+
         //generating a random Hex color code 
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
         randomHex = `#${randomHex.padStart(6,"0")}`;
@@ -13,10 +14,19 @@ const generatePalette = () => {
         color.classList.add("color");
         color.innerHTML = `<div class="color-box" style="background: ${randomHex}"></div>
         <span class="hex-value">${randomHex}</span>`
+
+        //adding click event to current li element to copy the color
+        color.addEventListener("click", () => copyColor(color, randomHex));
         palettes.appendChild(color);
     }
-
 }
 generatePalette();
+
+const copyColor = (elem, hexVal) => {
+    const colorElement = elem.querySelector(".hex-value");
+    navigator.clipboard.writeText(hexVal).then( () => {
+        colorElement.innerText = "copied";
+    })
+}
 
 refreshBtn.addEventListener("click", generatePalette);
